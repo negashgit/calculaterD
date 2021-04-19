84c13d38993e24c4cde51a58411e9d2275c20388
negashwe <negashsoft@hotmail.com>
2021-04-19 01:00:30 -0400

    commit my first code

---
+++ calcutatorProject/src/stc/classof2021/calculaterClass.java
@@ -0,0 +1,10 @@
+package stc.classof2021;
+
+public class calculaterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello, world");
+	}
+
+}
